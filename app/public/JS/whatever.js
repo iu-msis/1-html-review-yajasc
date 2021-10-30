0b61a349545f399beaaf5d5579de6809d1ad4181
yajasc <yajasc@gmail.com>
2021-10-30 16:08:25 -0400

    Added edit delete buttons and functionality

--- app/public/JS/whatever.js
+++ app/public/JS/whatever.js
@@ -1,9 +1,10 @@
 const SomeApp = {
     data() {
       return {
-        students: [],
-        offerForm: {},
-        offers:[]
+        "students": [],
+        "offerForm": {},
+        "offers":[],
+        selectedOffer : null
       }
     },
     computed: {},
@@ -32,6 +33,13 @@ const SomeApp = {
                 console.error(error);
             });
     },
+    postOffer(evt) {
+      if (this.selectedOffer === null) {
+          this.postNewOffer(evt);
+      } else {
+          this.postEditOffer(evt);
+      }
+    },
     postNewOffer(evt) {        
         
         console.log("Posting!", this.offerForm);
@@ -51,8 +59,68 @@ const SomeApp = {
             
             // reset the form
             this.offerForm = {};
+            this.resetOfferForm();
+
           });
+      },
+      postEditOffer(evt) {
+        // this.offerForm.studentId = this.selectedStudent.id;
+        this.offerForm.id = this.selectedOffer.id;
+
+        // console.log("Updating!", this.offerForm);
+
+        fetch('api/student/update.php', {
+            method:'POST',
+            body: JSON.stringify(this.offerForm),
+            headers: {
+              "Content-Type": "application/json; charset=utf-8"
+            }
+          })
+          .then( response => response.json() )
+          .then( json => {
+            console.log("Returned from post:", json);
+            // TODO: test a result was returned!
+            this.students = json;
+
+            // reset the form
+            this.resetOfferForm();
+          });
+      },
+
+      postDeleteOffer(o) {
+        if (!confirm("Are you sure you want to delete "+o.title+"?")) {
+          return;
+        }
+        console.log("Delete!", o);
+
+        fetch('api/student/delete.php', {
+            method:'POST',
+            body: JSON.stringify(o),
+            headers: {
+              "Content-Type": "application/json; charset=utf-8"
+            }
+          })
+          .then( response => response.json() )
+          .then( json => {
+            console.log("Returned from post:", json);
+            // TODO: test a result was returned!
+            this.students = json;
+
+            // reset the form
+            this.resetOfferForm();
+          });
+      },
+
+      selectOfferToEdit(o) {
+          this.selectedOffer = o;
+          this.offerForm = Object.assign({}, this.selectedOffer);
+      },
+
+      resetOfferForm() {
+          this.selectedOffer = null;
+          this.offerForm = {};
       }
+
   },
     created() {
         this.fetchStudentData();
